afd0ff311949c8a2fcb4dbf789f106849967410b
adityofr9 <adityofr9@gmail.com>
2022-07-13 10:28:24 +0700

    Memindahkan fungsi link sebelum http.createserver

--- app.js
+++ app.js
@@ -1,62 +1,31 @@
 const http = require('http');
 const fs = require('fs');
 
+const page = (pageLink, res) => {
+    fs.readFile(pageLink, (err, data) => {
+        if(err){
+            res.writeHead(404);
+            res.write('Error : page not found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+}
+
 http
     .createServer((req, res) => {
         const url = req.url;
         console.log(url);
 
-        // res.writeHead(200, {'Content-Type': 'text/plain'});
-
-        const page = (pageLink) => {
-            fs.readFile(pageLink, (err, data) => {
-                if(err){
-                    res.writeHead(404);
-                    res.write('Error : page not found');
-                } else {
-                    res.write(data);
-                }
-                res.end();
-            })
-        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
 
         if(url === '/about'){
-            // res.write('<h1>This is about page</h1>');
-            // fs.readFile('./about.html', (err, data) => {
-            //     if(err){
-            //         res.writeHead(404);
-            //         res.write('Error : page not found');
-            //     } else {
-            //         res.write(data);
-            //     }
-            //     res.end();
-            // })
-            page('./about.html');
+            page('./about.html', res);
         } else if (url === '/contact') {
-            // res.write('<h2>This is contact page</h2>');
-            // fs.readFile('./contact.html', (err, data) => {
-            //     if(err){
-            //         res.writeHead(404);
-            //         res.write('Error : page not found');
-            //     } else {
-            //         res.write(data);
-            //     }
-            //     res.end();
-            // })
-            page('./contact.html');
+            page('./contact.html', res);
         } else {
-            // res.write('Hello World!');
-            // fs.readFile('./index.html', (err, data) => {
-            //     if(err){
-            //         res.writeHead(404);
-            //         res.write('Error : page not found');
-            //     } else {
-            //         res.write(data);
-            //     }
-            //     res.end();
-            // })
-            page('./index.html');
-            
+            page('./index.html', res);
             
         }
 
